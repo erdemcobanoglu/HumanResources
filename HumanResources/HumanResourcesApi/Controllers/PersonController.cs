f95506b002554d0b883949fa1bb4d661a9e9afc6
erdem <45831228+erdemcobanoglu@users.noreply.github.com>
2023-04-15 22:44:16 +0300

    ilk calisan örnek

--- HumanResources/HumanResourcesApi/Controllers/PersonController.cs
+++ HumanResources/HumanResourcesApi/Controllers/PersonController.cs
@@ -3,18 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using HumanResourcesApi.Models;
+using HumanResourcesApi.Repositories;
 
 namespace HumanResourcesApi.Controllers
 {
-    [Route("api/[controller]")]
+    // https://localhost:44341/api/persons
+    [Route("api/persons")]
     [ApiController]
     public class PersonController : ControllerBase
     {
+        private readonly IPersonRepository _personRepository;
+        private readonly IMapper _mapper;
+
+        public PersonController(IPersonRepository personRepository,
+            IMapper mapper)
+        {
+            _personRepository = personRepository ??
+                                throw new ArgumentNullException(nameof(personRepository));
+            _mapper = mapper ??
+                      throw new ArgumentNullException(nameof(mapper));
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetPersons()
         {
-            return new string[] { "value1", "value2" };
+            var cityEntities = _personRepository.GetPersons();
+
+            return Ok(_mapper.Map<IEnumerable<PersonWithoutSkillsDto>>(cityEntities));
         }
 
         // GET api/values/5
---
+++ HumanResources/HumanResourcesApi/Migrations/20230415193610_HumanResources.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HumanResourcesApi.Migrations
+{
+    public partial class HumanResources : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    EMail = table.Column<string>(maxLength: 50, nullable: true),
+                    PhoneNumber = table.Column<string>(maxLength: 50, nullable: true),
+                    Description = table.Column<string>(maxLength: 200, nullable: true),
+                    OpenToWork = table.Column<bool>(nullable: false),
+                    Status = table.Column<string>(maxLength: 50, nullable: true),
+                    FirmName = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(maxLength: 200, nullable: true),
+                    PersonId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Skills_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Persons",
+                columns: new[] { "Id", "Description", "EMail", "FirmName", "Name", "OpenToWork", "PhoneNumber", "Status" },
+                values: new object[] { 1, "The one with that big park.", "test@gmail.com", "Microsoft", "New York City", true, "121321111", "StatusId" });
+
+            migrationBuilder.InsertData(
+                table: "Skills",
+                columns: new[] { "Id", "Description", "Name", "PersonId" },
+                values: new object[] { 1, "About C#", "C#", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Skills",
+                columns: new[] { "Id", "Description", "Name", "PersonId" },
+                values: new object[] { 2, "About java", "Java", 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_PersonId",
+                table: "Skills",
+                column: "PersonId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Skills");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
